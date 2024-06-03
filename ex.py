0a285a7f108fc5f7fcd951331ecd14839157cda9
ttaebarii <taehotuna@naver.com>
2024-06-03 17:01:24 +0900

    samsung img

--- ex.py
+++ ex.py
@@ -1,32 +1,24 @@
-# DFS
+import turtle
+import time
 
-# 방문 정보를 리스트 자료형으로 표현
-visited =[False] * 9
+wr = turtle.Turtle()
 
-# 각 노드가 연결된  정보를 리스트 자료형으로 표현 (2차원 리스트)
-graph = [
-    [],                
-    [2, 3, 8],          # 1번 노드와 연결된 노드들
-    [1, 7],             # 2번 노드와 연결된 노드들
-    [1, 4, 5],          # 3번 노드와 연결된 노드들
-    [3, 5],             # 4번 노드와 연결된 노드들
-    [3, 4],             # 5번 노드와 연결된 노드들
-    [7],                # 6번 노드와 연결된 노드들
-    [2, 6, 8],          # 7번 노드와 연결된 노드들
-    [1, 7]              # 8번 노드와 연결된 노드들
-]
+wr.fillcolor('red')
 
+wr.begin_fill()
 
-def DFS(graph, v, visited):
-    # 현재 노드를 방문 처리
-    visited[v] = True
-    print(v, end = ' ')
+wr.left(140)
+wr.forward(113)
 
-    for i in graph[v]:
-        if not visited[i]:
-            DFS(graph, i, visited)
-            print("hi")
+for i in range(200):
+    wr.right(1)
+    wr.forward(1)
+wr.left(120)
 
-# 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-
-DFS(graph, 1, visited)
+for i in range(200):
+    wr.right(1)
+    wr.forward(1)
+wr.forward(112)
+wr.end_fill
+time.sleep(100)
+wr.ht()
--- naver.py
+++ naver.py
@@ -1,18 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-query = "반도체"
-response = requests.get("https://search.naver.com/search.naver?ssc=tab.news.all&where=news&sm=tab_jum&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90")
+#response == 네이버 기사
+#response_2 == 한국경제 기사
+#response_3 == 삼성전자 뉴스룸
 
-# Parse HTML content using BeautifulSoup
-soup = BeautifulSoup(response.text, 'html.parser')
+query = "삼성전자"
 
-# Extract relevant text snippets
-links = soup.select('.news_tit')
-
-# Save the results to a text file
 with open('C:\\Users\\DC\\Desktop\\vscode_project\\QnA_project\\text.txt', 'a', encoding='utf-8') as file:
+    response = requests.get(f"https://search.naver.com/search.naver?ssc=tab.news.all&where=news&sm=tab_jum&query={query}")
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.select('.news_tit')  
     for link in links:
-        file.write(link.text + '\n\n')
-
-
+            file.write(link.text + '\n')
+    
+    for num in range(2,4):
+        response_3 = requests.get(f'https://news.samsung.com/kr/category/%ea%b8%b0%ec%97%85/page/{num}')
+        soup_3 = BeautifulSoup(response_3.text, 'html.parser')
+        links_3 = soup_3.select(".title.ellipsis")
+        for link_3 in links_3:
+            file.write(link_3.text + '\n') 
+    # for page in range(1,11):      
+    #     response_2 = requests.get(f'https://search.hankyung.com/search/news?query={query}&mediaid_clust=HKPAPER,HKCOM&page={page}') 
+    #     soup_2 = BeautifulSoup(response_2.text, 'html.parser')
+    #     links_2 = soup_2.select(".tit")
+    #     for link_2 in links_2:
+    #         file.write(link_2.text + '\n')  
+    
